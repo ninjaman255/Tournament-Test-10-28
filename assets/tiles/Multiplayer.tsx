9f73aa0ffc0292d12bc4436a173b0349615821e4
Ninjaman255 <aaronclevette@gmail.com>
2025-11-01 12:27:03 -0500

    Updated map + tileset to the new one. Thanks again Quetzen! These tiles and this map look fantastic!

--- assets/tiles/Multiplayer.tsx
+++ assets/tiles/Multiplayer.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.5" name="Multiplayer" tilewidth="64" tileheight="56" tilecount="52" columns="4" objectalignment="top">
+<tileset version="1.10" tiledversion="1.11.2" name="Multiplayer" tilewidth="64" tileheight="56" tilecount="56" columns="4" objectalignment="top">
  <tileoffset x="0" y="24"/>
- <image source="Multiplayer.png" width="256" height="728"/>
+ <image source="Multiplayer.png" width="256" height="784"/>
 </tileset>
